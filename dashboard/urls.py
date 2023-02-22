10610049c7da14c61276b2307bdf872746ce7665
Abdulbosit <101085020+AbdulbositGo@users.noreply.github.com>
2023-02-22 15:31:55 +0500

    dashboard changed

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 
-from .views import DashboardView
+from .views import (
+    DashboardPostsView, DashboardFollowingTagsView,
+    DashboardFollowingUsersView, DashboardFollowersView,
+    DashboardReadingListView
+)
 
+app_name = "dashboard"
 
 urlpatterns = [
-    path('post/', DashboardView.as_view(), name='dashboard'),
+    path('post/', DashboardPostsView.as_view(), name='posts'),
+    path('folllowing-tags/', DashboardFollowingTagsView.as_view(), name='following-tags'),
+    path('followers/', DashboardFollowersView.as_view(), name='followers'),
+    path('following-users/', DashboardFollowingUsersView.as_view(), name='following-users'),
+    path('reading-list/', DashboardReadingListView.as_view(), name='reading-list'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -5,6 +5,26 @@ from django.views import View
 # Create your views here.
 
 
-class DashboardView(View):
+class DashboardPostsView(View):
     def get(self, request):
-        return render(request, 'dashboard.html')
+        return render(request, 'dashboard/posts.html')
+
+
+class DashboardFollowingTagsView(View):
+    def get(self, request):
+        return render(request, 'dashboard/following-tags.html')
+
+
+class DashboardFollowersView(View):
+    def get(self, request):
+        return render(request, 'dashboard/followers.html')
+
+
+class DashboardFollowingUsersView(View):
+    def get(self, request):
+        return render(request, 'dashboard/following-users.html')
+
+
+class DashboardReadingListView(View):
+    def get(self, request):
+        return render(request, 'dashboard/reading-list.html')
