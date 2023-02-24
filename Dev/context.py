8858b316c2dfb80a3f545f79f71e85989e76ccb8
Abdulbosit <101085020+AbdulbositGo@users.noreply.github.com>
2023-02-24 19:01:00 +0500

    post temlpate changed

--- Dev/context.py
+++ Dev/context.py
@@ -5,11 +5,14 @@ from users.models import Follow
 
 def dashboard_count_info(request):
     if request.user.is_authenticated:
+        following = request.user.following.all()
+        following_username = [user.following.username for user in following]
         context = {
             'post_count': Post.objects.filter(profile=request.user).count(),
             'following_tag_count': FollowingTags.objects.filter(profile=request.user).count(),
             'following_count': Follow.objects.filter(follower=request.user).count(),
             'follower_count': Follow.objects.filter(following=request.user).count(),
+            'following_username': following_username
         }
         return context
     return {}
